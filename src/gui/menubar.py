4c3e8d12483e091d1e5ec5d94e83d57cb2f94b70
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-22 17:31:14 +0200

    Menu item: new fractal

--- src/gui/menubar.py
+++ src/gui/menubar.py
@@ -21,6 +21,12 @@ class Menubar(tkinter.Menu):
                                   command=parent.quit)
 
 
+        self.renderermenu = tkinter.Menu(self, tearoff=0)
+        self.renderermenu.add_command(label="New fractal", image=main_window.icons.fractal_new_icon,
+                                     compound="left", underline=0, accelerator="Ctrl+N")
+
+        self.compositormenu = tkinter.Menu(self, tearoff=0)
+
         self.helpmenu = tkinter.Menu(self, tearoff=0)
         self.helpmenu.add_command(label="Help",
                                   image=main_window.icons.help_faq_icon,
@@ -32,6 +38,8 @@ class Menubar(tkinter.Menu):
                                   compound="left", underline=0, command=about)
 
         self.add_cascade(label="File", menu=self.filemenu, underline=0)
+        self.add_cascade(label="Renderer", menu=self.renderermenu, underline=0)
+        self.add_cascade(label="Compositor", menu=self.compositormenu, underline=0)
         self.add_cascade(label="Help", menu=self.helpmenu, underline=0)
 
         self.parent.bind('<F1>', lambda event: help())
