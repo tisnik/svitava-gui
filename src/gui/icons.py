aa8d6b8872f5e73acc95e607d59e408b1a504730
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-07 09:43:34 +0200

    References to three new icons

--- src/gui/icons.py
+++ src/gui/icons.py
@@ -6,6 +6,10 @@ import icons.application_exit
 import icons.help_faq
 import icons.help_about
 import icons.fractal_new
+import icons.file_open
+import icons.file_save
+import icons.file_save_as
+import icons.edit
 
 
 class Icons:
@@ -17,3 +21,9 @@ class Icons:
         self.help_faq_icon = tkinter.PhotoImage(data=icons.help_faq.icon)
         self.help_about_icon = tkinter.PhotoImage(data=icons.help_about.icon)
         self.fractal_new_icon = tkinter.PhotoImage(data=icons.fractal_new.icon)
+
+        self.file_open_icon = tkinter.PhotoImage(data=icons.file_open.icon)
+        self.file_save_icon = tkinter.PhotoImage(data=icons.file_save.icon)
+        self.file_save_as_icon = tkinter.PhotoImage(data=icons.file_save_as.icon)
+
+        self.edit_icon = tkinter.PhotoImage(data=icons.edit.icon)
