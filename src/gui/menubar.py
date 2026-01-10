a8864e3a3ba06d2de0b69fa9f7fe7a23dd72db22
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-10 09:31:54 +0100

    Functions to open fractal type dialogs

--- src/gui/menubar.py
+++ src/gui/menubar.py
@@ -5,6 +5,10 @@ import tkinter
 from gui.dialogs.about_dialog import about
 from gui.dialogs.help_dialog import help
 from gui.dialogs.fractal_type_dialog import select_fractal_type_dialog
+from gui.dialogs.pattern_type_dialog import select_pattern_type_dialog
+from gui.dialogs.filter_type_dialog import select_filter_type_dialog
+from gui.dialogs.color_mapper_dialog import select_color_mapper_dialog
+from gui.dialogs.settings_dialog import settings_dialog
 
 from configuration import Configuration
 
@@ -27,6 +31,7 @@ class Menubar(tkinter.Menu):
         menu_font = font = ("", configuration.menu_font_size)
 
         self.parent = parent
+        self.icons = icons
         self.main_window = main_window
 
         self.file_menu = tkinter.Menu(self, tearoff=0, font=menu_font)
@@ -46,16 +51,21 @@ class Menubar(tkinter.Menu):
         self.renderer_menu.add_separator()
         self.renderer_menu.add_command(label="New pattern", image=icons.pattern_new_icon,
                                      compound="left", underline=0, accelerator="Ctrl+P",
-                                     command=self.command_fractal_type_dialog)
+                                     command=self.command_pattern_type_dialog)
+
+        self.processor_menu = tkinter.Menu(self, tearoff=0, font=menu_font)
+        self.processor_menu.add_command(label="New filter", image=icons.filter_new_icon,
+                                     compound="left", underline=4, accelerator="Ctrl+F",
+                                     command=self.command_filter_type_dialog)
+        self.processor_menu.add_separator()
+        self.processor_menu.add_command(label="New color mapper", image=icons.fill_color_icon,
+                                     compound="left", underline=4, accelerator="Ctrl+C",
+                                     command=self.command_color_mapper_dialog)
 
         self.compositor_menu = tkinter.Menu(self, tearoff=0, font=menu_font)
-        self.compositor_menu.add_command(label="New filter", image=icons.filter_new_icon,
-                                     compound="left", underline=4, accelerator="Ctrl+F",
+        self.compositor_menu.add_command(label="New connection", image=icons.draw_arrow_forward_icon,
+                                     compound="left", underline=4, accelerator="Ctrl+A",
                                      command=None)
-        self.compositor_menu.add_command(label="New color mapper", image=icons.fill_color_icon,
-                                     compound="left", underline=4, accelerator="Ctrl+C",
-                                     command=None)
-        self.compositor_menu.add_separator()
 
         self.palette_menu = tkinter.Menu(self, tearoff=0, font=menu_font)
         self.palette_menu.add_command(label="Load palette", image=icons.file_open_icon,
@@ -64,6 +74,12 @@ class Menubar(tkinter.Menu):
                                     compound="left", underline=0)
         self.palette_menu.add_command(label="Palette editor", image=icons.edit_icon,
                                     compound="left", underline=0)
+
+        self.settings_menu = tkinter.Menu(self, tearoff=0, font=menu_font)
+        self.settings_menu.add_command(label="Configuration",
+                                  image=icons.configure_icon,
+                                  compound="left", underline=0, accelerator="F9",
+                                  command=self.settings_dialog)
 
         self.help_menu = tkinter.Menu(self, tearoff=0, font=menu_font)
         self.help_menu.add_command(label="Help",
@@ -77,8 +93,10 @@ class Menubar(tkinter.Menu):
 
         self.add_cascade(label="File", menu=self.file_menu, underline=0, font = main_menu_font)
         self.add_cascade(label="Renderer", menu=self.renderer_menu, underline=0, font = main_menu_font)
+        self.add_cascade(label="Processor", menu=self.processor_menu, underline=0, font = main_menu_font)
         self.add_cascade(label="Compositor", menu=self.compositor_menu, underline=0, font = main_menu_font)
-        self.add_cascade(label="Palette", menu=self.palette_menu, underline=0, font = main_menu_font)
+        self.add_cascade(label="Palette", menu=self.palette_menu, underline=1, font = main_menu_font)
+        self.add_cascade(label="Settings", menu=self.settings_menu, underline=0, font = main_menu_font)
         self.add_cascade(label="Help", menu=self.help_menu, underline=0, font = main_menu_font)
 
         self.parent.bind('<F1>', lambda event: help())
@@ -89,6 +107,38 @@ class Menubar(tkinter.Menu):
         """
         Open the fractal-type selection dialog.
         
-        Displays the dialog that lets the user choose a fractal or pattern type to create.
+        Displays the dialog that lets the user choose a fractal type to create.
         """
-        select_fractal_type_dialog(self.parent)
+        select_fractal_type_dialog(self.parent, self.icons)
+
+    def command_pattern_type_dialog(self):
+        """
+        Open the pattern selection dialog.
+        
+        Displays the dialog that lets the user choose a pattern type to create.
+        """
+        select_pattern_type_dialog(self.parent)
+
+    def command_filter_type_dialog(self):
+        """
+        Open the filter selection dialog.
+        
+        Displays the dialog that lets the user choose a filter type to create.
+        """
+        select_filter_type_dialog(self.parent)
+
+    def command_color_mapper_dialog(self):
+        """
+        Open the color mapper selection dialog.
+        
+        Displays the dialog that lets the user choose a color mapper to create.
+        """
+        select_color_mapper_dialog(self.parent)
+
+    def settings_dialog(self):
+        """
+        Open the settings dialog.
+        
+        Displays the dialog with configuration/settings.
+        """
+        settings_dialog(self.parent)
