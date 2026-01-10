07748992022c386b12608d85e8f0859bfba69f88
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-10 09:31:19 +0100

    Stub for image renderer

---
+++ src/renderer.py
@@ -0,0 +1,13 @@
+import os.path
+import subprocess
+
+def cached_image(path: str):
+    if not os.path.isfile(path):
+        print("Rendering started")
+        popen = subprocess.Popen(["./a.out"], stdout=subprocess.PIPE)
+        popen.wait()
+    else:
+        print("Reusing cached image")
+    with open(path, "rb") as fin:
+        image_data = fin.read()
+    return image_data
