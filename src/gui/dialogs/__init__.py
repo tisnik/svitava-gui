248f9fbccff2940953930e9fb0450bce2713523c
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-01-17 09:15:11 +0100

    Added license header

--- src/gui/dialogs/__init__.py
+++ src/gui/dialogs/__init__.py
@@ -1,1 +1,14 @@
 """Implementation of all GUI dialog boxes."""
+
+#
+#  (C) Copyright 2019, 2026  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
